fe4636f06625b08ef3d67f5b53406a499edb09bc
Yongting <yongtingshi657@gmail.com>
2025-07-10 23:54:20 -0400

    Describe your changes

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Header from "./assets/Header"
+import Content from "./assets/Content"
+
+function App() {
+
+  return (
+    <>
+     <Header />
+     <Content />
+    </>
+  )
+}
+
+export default App
---
+++ src/assets/Header.jsx
@@ -0,0 +1,15 @@
+export default function Header(){
+    return (
+        <>
+        <header>
+            <img></img>
+            <h2>Yongting Shi</h2>
+            <p>Frontend Developer</p>
+            <p><a href="#">My website</a></p>
+            <button><a href="#">Email</a></button>
+            <button><a href="#">LinkedIn</a></button>
+
+        </header>
+        </>
+    )
+}
